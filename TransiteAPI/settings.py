aefd8e3a0fd46e2ab80f0c475b1c5a9d71da4ad2
Farid Habibli <80222517+Ferid612@users.noreply.github.com>
2022-06-30 01:10:07 +0400

    test

--- TransiteAPI/settings.py
+++ TransiteAPI/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@yh-b226#qga!z$j=p)h2$ga2#+!cevywtgb-!)ev(n!g=e&x3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- TransiteAPI/urls.py
+++ TransiteAPI/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from .import settings
 urlpatterns = [
     path('', include('TransiteApp.urls')),
+    
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
